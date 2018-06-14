d9772e391faf3c000fa844707c3dded380882f99
Edward <ed828a@gmail.com>
2018-06-14 23:26:24 +0930

    Github query implementation

---
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.codelabs.paging.data
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.paging.LivePagedListBuilder
+import android.util.Log
+import com.example.android.codelabs.paging.api.GithubService
+import com.example.android.codelabs.paging.api.searchRepos
+import com.example.android.codelabs.paging.db.GithubLocalCache
+import com.example.android.codelabs.paging.model.RepoSearchResult
+
+/**
+ * Repository class that works with local and remote data sources.
+ */
+class GithubRepository(private val service: GithubService, private val cache: GithubLocalCache) {
+
+    /**
+     * Search repositories whose names match the query.
+     */
+    fun search(query: String): RepoSearchResult {
+        Log.d("GithubRepository", "New query: $query")
+
+        val repoBoundaryCallback = RepoBoundaryCallback(query, service, cache)
+
+        // Get data source factory from the local cache
+        val dataSourceFactory = cache.reposByName(query)
+        //Get the Paged List
+        val data = LivePagedListBuilder(dataSourceFactory, DATABASE_PAGE_SIZE)
+                .setBoundaryCallback(repoBoundaryCallback)
+                .build()
+
+        //Get the network errors exposed by the boundary callback
+        return RepoSearchResult(data, repoBoundaryCallback.networkErrors)
+    }
+
+//    fun requestMore(query: String) {
+//        requestAndSaveData(query)
+//    }
+
+
+
+    companion object {
+        private const val DATABASE_PAGE_SIZE = 20
+    }
+}
